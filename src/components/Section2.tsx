bc78f66cb4dfc3b7a7f3917951da15cb30116b56
Tung <tung@Tungs-MacBook-Pro.local>
2023-08-22 11:32:57 -0700

    ip test

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,48 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import get from "lodash/get";
-
-type ApiResponse = {
-  ip: string;
-};
-
-class MyClassComponent extends React.Component {
-  myIp: string | null = null;
-
-  getMyIp = () => {
-    fetch("https://api.ipify.org?format=json")
-      .then((response) => response.json())
-      .then((res: ApiResponse) => {
-        this.myIp = get(res, "ip");
-      })
-      .catch((err: Error) => console.error("Problem fetching my IP", err));
-  };
-
-  componentDidMount() {
-    this.getMyIp();
-  }
-
-  render() {
-    return (
-      <>
-        <div>
-          My IP: {this.myIp} {/* Render the myIp value here */}
-        </div>
-      </>
-    );
-  }
-}
 
 export default function Section2() {
   const { t } = useTranslation();
+
+  const [ipAddress, setIPAddress] = useState("");
+
+  useEffect(() => {
+    fetch("https://api64.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => setIPAddress(data.ip))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
       <div className="mt-4 w-screen">
         <div className="flex justify-center">
           <p className="text-2xl font-semibold">
-            {t("popularService")} {}
+            {t("popularService")} {ipAddress}
           </p>
-          <MyClassComponent />
         </div>
       </div>
     </>
