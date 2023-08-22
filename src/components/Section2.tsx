7d9c39e596955b5d920cb72eee69afd087a50db4
Tung <tung@Tungs-MacBook-Pro.local>
2023-08-22 11:36:39 -0700

    fix

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+interface IPAddressResponse {
+  ip: string;
+}
+
 export default function Section2() {
   const { t } = useTranslation();
 
-  const [ipAddress, setIPAddress] = useState("");
+  const [ipAddress, setIPAddress] = useState<string>("");
 
   useEffect(() => {
     fetch("https://api64.ipify.org?format=json")
       .then((response) => response.json())
-      .then((data) => setIPAddress(data.ip))
+      .then((data: IPAddressResponse) => setIPAddress(data.ip)) // Using the defined interface here
       .catch((error) => console.log(error));
   }, []);
 
