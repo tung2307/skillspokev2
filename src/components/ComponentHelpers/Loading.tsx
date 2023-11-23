52e891679d287283b260c97f116521a93bd757f6
Tung Nguyen <tung.nguyen23797@gmail.com>
2023-11-22 17:26:06 -0800

    add Loading component

---
+++ src/components/ComponentHelpers/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-solid border-blue-500"></div>
+    </div>
+  );
+}
--- src/pages/search/[params].tsx
+++ src/pages/search/[params].tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
+import Loading from "~/components/ComponentHelpers/Loading";
 import SearchResult from "~/components/SearchPage/SearchResult";
 import { api } from "~/utils/api";
 
@@ -15,52 +16,58 @@ export default function Search() {
     service: serviceValue ?? "",
     location: districtValue ?? "",
   });
-  if (isLoading) {
-    return <div></div>; // This can be a spinner or any other loading indicator you'd prefer.
-  }
+
   const serviceTranslation = t(`services.${serviceValue}`);
 
   return (
     <>
-      <div className="flex flex-col pb-2 md:border-b md:pb-10">
-        {searchResult?.length ? (
-          <>
-            <div className="flex w-full justify-center pb-5 md:pb-10 md:pt-10">
-              {searchResult.map((result, index) => (
-                <SearchResult key={index} data={result} />
-              ))}
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="w-full pb-10">
-              <div className="p-5 md:p-10">
-                <div className="text-base font-bold md:text-xl">
-                  {t("noServiceFound", {
-                    service: serviceTranslation,
-                    district: districtValue!,
-                  })}
+      {isLoading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col pb-2 md:border-b md:pb-10">
+            {searchResult?.length ? (
+              <>
+                <div className="flex w-full justify-center pb-5 md:pb-10 md:pt-10">
+                  {searchResult.map((result, index) => (
+                    <SearchResult key={index} data={result} />
+                  ))}
                 </div>
+              </>
+            ) : (
+              <>
+                <div className="w-full pb-10">
+                  <div className="p-5 md:p-10">
+                    <div className="text-base font-bold md:text-xl">
+                      {t("noServiceFound", {
+                        service: serviceTranslation,
+                        district: districtValue!,
+                      })}
+                    </div>
 
-                <div>{t("findClose")} </div>
-              </div>
+                    <div>{t("findClose")} </div>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <p className="mb-3 mt-3 w-[90%] text-center">{t("addStore")}</p>
             </div>
-          </>
-        )}
-      </div>
-      <div>
-        <div className="flex justify-center">
-          <p className="mb-3 mt-3 w-[90%] text-center">{t("addStore")}</p>
-        </div>
 
-        <div className="flex justify-center">
-          <Link href="/add/store">
-            <p className="flex w-40 justify-center rounded-lg border bg-[#4682B4] p-2 text-white hover:bg-white hover:text-[#4682B4]">
-              {t("create")}
-            </p>
-          </Link>
-        </div>
-      </div>
+            <div className="flex justify-center">
+              <Link href="/add/store">
+                <p className="flex w-40 justify-center rounded-lg border bg-[#4682B4] p-2 text-white hover:bg-white hover:text-[#4682B4]">
+                  {t("create")}
+                </p>
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
